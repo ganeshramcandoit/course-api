30d074c92ce8416033eb595afe5832ec0fcee7ea
ganeshramcandoit <ganeshramcandoit@gmail.com>
2017-07-27 23:20:39 +0530

    Lesson 20

--- src/main/java/io/javabrains/springbootstarter/topic/TopicService.java
+++ src/main/java/io/javabrains/springbootstarter/topic/TopicService.java
@@ -3,6 +3,8 @@ package io.javabrains.springbootstarter.topic;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.OptionalInt;
+import java.util.stream.IntStream;
 
 import org.springframework.stereotype.Service;
 
@@ -20,8 +22,18 @@ public class TopicService {
 	public Topic getTopic(String id) {
 		return topics.stream().filter(t -> t.getId().equalsIgnoreCase(id)).findFirst().get();
 	}
-	
+
 	public void addTopic(Topic topic) {
 		topics.add(topic);
+	}
+
+	public void updateTopic(Topic topic, String id) {
+		OptionalInt indexOpt = IntStream.range(0, topics.size()).filter(i -> id.equals(topics.get(i).getId()))
+				.findFirst();
+		topics.set(indexOpt.getAsInt(), topic);
+	}
+
+	public void deleteTopic(String id) {
+		topics.removeIf(t -> t.getId().equals(id));
 	}
 }
